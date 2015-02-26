f7336fad6cdc90da851e7874f17e917f0777a093
joannachia <chia.joa@gmail.com>
2015-02-25 18:18:03 -0800

    changes in itemServlet.java

--- src/edu/ucla/cs/cs144/ItemServlet.java
+++ src/edu/ucla/cs/cs144/ItemServlet.java
@@ -13,6 +13,30 @@ public class ItemServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+        String results = AuctionSearchClient.getXMLDataForItemId(request.getParameter("id"));
+
+
+
+
+        request.setAttribute("itemId", results);		//string
+        request.setAttribute("itemName", results);		//string
+        request.setAttribute("categories", results);	//array of strings
+        request.setAttribute("currently", results);		//string
+        request.setAttribute("buyPrice", results);		//string
+        request.setAttribute("firstBid", results);		//string
+        request.setAttribute("num_bids", results);		//string
+        request.setAttribute("bids", resulst);			//array of bid types
+        request.setAttribute("location", location);		//location type
+        request.setAttribute("country", country);		//string
+        request.setAttribute("starts", starts);			//string
+        request.setAttribute("ends", ends);				//string
+        request.setAttribute("seller", seller);			//seller type
+
+
+
+        RequestDispatcher requestDispatcher; 
+		requestDispatcher = request.getRequestDispatcher("/itemResult.jsp");
+		requestDispatcher.forward(request, response);
+
     }
 }
