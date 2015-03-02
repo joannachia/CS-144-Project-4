509614e606e1c4d21eeebab93ed4cdb20eba6847
joannachia <chia.joa@gmail.com>
2015-03-01 18:32:49 -0800

    building proxy on proxyServlet.java

--- src/edu/ucla/cs/cs144/ProxyServlet.java
+++ src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -12,8 +12,38 @@ public class ProxyServlet extends HttpServlet implements Servlet {
        
     public ProxyServlet() {}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    	throws ServletException, IOException
     {
-        // your codes here
+        
+    	try {
+	        String forwardURLString = request.getParameter("fwdurl");
+	        if(forwardURLString == null)
+	        	forwardURLString="http://localhost:1448/eBay/";
+
+	        URL forwardURL = new URL(forwardURLString);
+	        HttpURLConnection forwardConnection = (HttpURLConnection) forwardURL.openConnection();
+
+	        for (String key : forwardConnection.getHeaderFields().keySet()) {
+	        	response.setHeader(key, forwardConnection.getHeaderField(key));
+	        }
+
+	        InputStream in = forwardConnection.getInputStream();
+	        OutputStream out = response.getOutputStream();
+
+
+	        int length = 0;
+	        byte[] buffer = new byte[1024];
+	        while( (length = in.read(buffer)) > 0) 
+	        	out.write(buffer, 0, length);
+	        
+
+	        in.close();
+	        out.close();
+		
+		} catch (Exception e){
+
+		}
+
     }
 }
